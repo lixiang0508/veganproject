07659a09eba5063967a7d7335f9e9fe5dc985c65
zlx <zlx980508@sina.com>
2022-02-03 22:57:30 +0800

    second commit

--- src/main/java/com/zlx/gradthesis/demo/DemoApplication.java
+++ src/main/java/com/zlx/gradthesis/demo/DemoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Let's go vegan!");
+		System.out.println("Let's go vegan!!");
+
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
